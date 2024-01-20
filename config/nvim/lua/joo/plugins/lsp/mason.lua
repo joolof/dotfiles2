61d664d954d4991516bfa49be51f3a964662c778
Johan Olofsson <jolofsso@hql026442.ads.eso.org>
2024-01-20 18:27:12 +0100

    nvim mostly

--- config/nvim/lua/joo/plugins/lsp/mason.lua
+++ config/nvim/lua/joo/plugins/lsp/mason.lua
@@ -22,8 +22,9 @@ mason.setup()
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"texlab",
 		"pyright",
+		"texlab",
+		"marksman",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
